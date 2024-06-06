5daab7be7c5ad7bd4e4035f9b2c467e7f0c3102c
creating00 <creatingsr@gmail.com>
2024-06-06 18:45:26 -0300

    se sube proyecto

---
+++ src/main/java/com/creating/ribota/RibotaApplication.java
@@ -0,0 +1,13 @@
+package com.creating.ribota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creating/ribota/RibotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creating.ribota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
