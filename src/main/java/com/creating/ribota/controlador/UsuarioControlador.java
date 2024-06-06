52e0f3183f702cb0233a7b4e83f5289c1971c0a0
creating00 <creatingsr@gmail.com>
2024-06-06 20:54:44 -0300

    vista html mas controller

--- src/main/java/com/creating/ribota/controlador/UsuarioControlador.java
+++ src/main/java/com/creating/ribota/controlador/UsuarioControlador.java
@@ -1,4 +1,14 @@
 package com.creating.ribota.controlador;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+@Controller
 public class UsuarioControlador {
+    @GetMapping(value = "/form")
+    public String crear(Map<String, Object> model) {
+        model.put("titulo", "Formulario de Usuario");
+        return "form";
+    }
 }
--- src/main/java/com/creating/ribota/servicio/UsuarioServicio.java
+++ src/main/java/com/creating/ribota/servicio/UsuarioServicio.java
@@ -1,5 +1,5 @@
 package com.creating.ribota.servicio;
 
 public class UsuarioServicio implements IUsuarioServicio{
-    
+
 }
